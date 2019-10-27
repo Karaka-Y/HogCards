08a44ad7ff14e9f723f1dd631d8f4080ac2a6434
Karaka-Y <D.karaka@protonmail.com>
2019-10-27 20:23:29 +0200

    Add the card game

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//describes a behaviour of all cards in the scene
+public class MemoryCard : MonoBehaviour {
+	
+	[SerializeField] private GameObject cardBack;
+	[SerializeField] private SceneController controller;
+	private int _id;
+	public int Id{
+		get {return _id;}
+	}
+	public void SetCard (int id, Sprite image){
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+	public void OnMouseDown() {
+		if(cardBack.activeSelf && controller.canReveal){
+			cardBack.SetActive(false);
+			controller.CardRevealed(this);
+		}
+	}
+	public void Unreveal() {
+		cardBack.SetActive(true);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+	[SerializeField] private GameObject targetObject;
+	[SerializeField] private string targetMessage;
+
+	public Color hightlightColor = Color.cyan;
+
+	public void OnMouseEnter(){
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		if(sprite != null){
+			sprite.color = hightlightColor;
+		}
+	}
+
+	public void OnMouseExit(){
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		if(sprite != null){
+			sprite.color = Color.white;
+		}
+	}
+
+	public void OnMouseDown(){
+		transform.localScale = new Vector3 (0.3f, 0.3f, 1);
+	}
+
+	public void OnMouseUp(){
+		
+		if(targetObject != null){
+			targetObject.SendMessage(targetMessage);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

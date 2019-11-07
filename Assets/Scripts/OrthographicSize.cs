f5b73d50ac33d6b8a53e60c9ffe2712015c026d0
Urii Shalya <D.karaka@protonmail.com>
2019-11-07 23:31:28 +0200

    Improved camera scaling
    Now I have created a new script that changes camera's orthographic size relatively to screen size. Now it automatically changes size with any resolutions (not tested with resolutions above full hd).

---
+++ Assets/Scripts/OrthographicSize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrthographicSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		float targetRatio = 1.8f;
+
+		if(screenRatio >= targetRatio){
+			Camera.main.orthographicSize = 5f;
+		}
+		else{
+			float differenceInSize = targetRatio / screenRatio;
+			Camera.main.orthographicSize = 5f * differenceInSize;
+		}
+
+	}
+}
